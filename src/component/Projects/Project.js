1aa28d964179a5eb8d44bb2fefac549c8af6c2cb
Md. Shakil Ahmed <44937746+shakil1206@users.noreply.github.com>
2020-12-24 13:32:20 +0600

    project card image on scroll design added

--- src/component/Projects/Project.js
+++ src/component/Projects/Project.js
@@ -6,7 +6,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 const Project = (props) => {
-    
+
     useEffect(() => {
         Aos.init({ duration: 2000 });
     }, [])
@@ -17,14 +17,16 @@ const Project = (props) => {
         <div className="col-12 col-sm-6 col-md-4">
             <div className="card-group card-style">
                 <div data-aos="flip-left" className="card">
-                    <img src={img} className="card-img-top" alt={title} />
+                    <div className="screen">
+                        <img src={img} className="card-img-top" alt={title} title="Hover on image to see full design!" />
+                    </div>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
                     </div>
                     <div className="card-footer">
-                        <a href={gitHub}><FaGithubSquare/></a>
-                        <a href={liveLink}><FiExternalLink/></a>
+                        <a href={gitHub}><FaGithubSquare /></a>
+                        <a href={liveLink}><FiExternalLink /></a>
                     </div>
                 </div>
             </div>
