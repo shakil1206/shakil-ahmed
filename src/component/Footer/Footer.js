dff6e0daac757214ebeb54515f7e7b1173751587
Md. Shakil Ahmed <44937746+shakil1206@users.noreply.github.com>
2021-01-29 12:28:19 +0600

    seo added

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,12 +3,17 @@ import './Footer.css';
 import { GoRepoForked } from 'react-icons/go';
 
 const Footer = () => {
+
+    const todays = new Date();
+    const year = todays.getFullYear();
+
+
     return (
         <div className='foooter'>
             <div className="container">
                 <div className="row copy">
                     <div className="col-12 col-sm-12 col-md-12 d-flex justify-content-center align-items-center justify-content-between">
-                        <small>Copyright &copy; 2020 Shakil Ahmed</small>
+                        <small>Copyright &copy; {year} Shakil Ahmed</small>
                         <div className="d-flex align-items-center dev">
                             <a href="https://github.com/shakil1206/shakil-ahmed" target="_blank" rel="noopener noreferrer" ><GoRepoForked /></a>
                             <h4> <span className="heart">&hearts;</span> Developed BY ME</h4>
